ebbf43157ea87f2527f79cf23b3cd5b25b8383ae
jiezz3 <yuanweijie0769@gmail.com>
2021-07-16 15:05:30 +0800

    导航栏布局

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,6 +1,13 @@
 <template>
-  <div>
-    Money
+  <div class="money">
+    <div class="content">
+      <p>monery</p>
+      <p>monery</p>
+      <p>monery</p>
+      <p>monery</p>
+      <p>monery</p>
+      <p>monery</p>
+    </div>
     <Nav />
   </div>
 </template>
@@ -8,5 +15,15 @@
 <script>
 export default {};
 </script>
-<style scoped>
-</style>
+<style lang="scss" scoped>
+.content {
+  flex-grow: 1;
+}
+.money {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  overflow: auto;
+}
+</style> 
+
