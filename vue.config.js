9fa1a90c649a18cc869352d0fb4bb00e5ed84b3e
jiezz3 <yuanweijie0769@gmail.com>
2021-08-11 15:13:18 +0800

    基本完成1.0

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 /* eslint-disable */
 const path = require('path')
 module.exports = {
+  publicPath: '',
   lintOnSave: false,
   chainWebpack: config => {
     const dir = path.resolve(__dirname, 'src/assets/icon')
